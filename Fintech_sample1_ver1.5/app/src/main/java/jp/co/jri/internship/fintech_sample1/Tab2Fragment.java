7f2fb19df952d807861da98f45798b6f96fd4d9c
kazuki-684 <kurikazu2876@gmail.com>
2023-12-15 02:40:38 +0900

    棒グラフ完了

--- Fintech_sample1_ver1.5/app/src/main/java/jp/co/jri/internship/fintech_sample1/Tab2Fragment.java
+++ Fintech_sample1_ver1.5/app/src/main/java/jp/co/jri/internship/fintech_sample1/Tab2Fragment.java
@@ -2,6 +2,7 @@ package jp.co.jri.internship.fintech_sample1;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,16 +11,25 @@ import android.view.ViewGroup;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,36 +51,35 @@ public class Tab2Fragment extends Fragment {
 
         return v;
     }
-    private TextView mTextView;
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // TextViewをひも付けます
-        mTextView = (TextView) view.findViewById(R.id.textView);
-        // Buttonのクリックした時の処理を書きます
-        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTextView.setText(mTextView.getText() + "!");
-            }
-        });
+        CombinedChart combinedChart = view.findViewById(R.id.combinedChart);
+        // 収入と支出のデータ（棒グラフ）
+        BarData barData = new BarData(getBarData(view)/* your BarDataSet */);
 
-        int sumWin = 1;
-        int sumLoss = 1;
-        int sumDraw = 1;
+// 収入と支出の和のデータ（折れ線グラフ）
+        LineData lineData = new LineData(getLineData(view));
 
-        // 棒グラフ表示領域を取得
-        chart = view.findViewById(R.id.barChart);
+// CombinedData にデータを追加
+        CombinedData combinedData = new CombinedData();
+        combinedData.setData(barData);
+        combinedData.setData(lineData);
 
-        // 棒グラフに勝敗数、あいこ数のデータをセット。getBarDataの関数は別途定義
-        BarData data = new BarData(getBarData(view,sumWin,sumLoss,sumDraw));
+// CombinedChart に CombinedData を設定
+        combinedChart.setData(combinedData);
+
+        //表示データ取得
+        BarData data = new BarData(getBarData(view));
         chart.setData(data);
 
         //Y軸(左)
         YAxis left = chart.getAxisLeft();
-        left.setAxisMinimum(0f);
-        left.setAxisMaximum(100000f);
+        left.setAxisMinimum(-600000);
+        left.setAxisMaximum(1000000);
+        left.setLabelCount(6);
         left.setDrawTopYLabelEntry(true);
+        //整数表示に
 
         //Y軸(右)
         YAxis right = chart.getAxisRight();
@@ -82,7 +91,7 @@ public class Tab2Fragment extends Fragment {
         //X軸
         XAxis xAxis = chart.getXAxis();
         //X軸に表示するLabelのリスト(最初の""は原点の位置)
-        final String[] labels = {"","消費", "収入", "テスト"};
+        final String[] labels = {"","7月", "8月", "9月"};
 
         xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
         XAxis bottomAxis = chart.getXAxis();
@@ -91,28 +100,32 @@ public class Tab2Fragment extends Fragment {
         bottomAxis.setDrawGridLines(false);
         bottomAxis.setDrawAxisLine(true);
 
-        // グラフ上の表示
+        //グラフ上の表示
         chart.setDrawValueAboveBar(true);
         chart.getDescription().setEnabled(false);
         chart.setClickable(false);
 
-        // 凡例を非表示
+        //凡例
         chart.getLegend().setEnabled(false);
 
-        chart.setScaleEnabled(false);
-        // アニメーション
+        chart.setScaleEnabled(true);
+        //アニメーション
         chart.animateY(1200, Easing.Linear);
+
     }
 
-    private List<IBarDataSet> getBarData(View view,int sumWin, int sumLoss, int sumDraw){
+    private List<IBarDataSet> getBarData(View view){
         // 表示させるデータをリストに格納
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(1, clickBtSearch(1)));
-        entries.add(new BarEntry(2, clickBtSearch(2)));
-        entries.add(new BarEntry(3, sumDraw));
+        int a = clickBtSearch(1);
+        int b = clickBtSearch(2);
 
-        TextView tvTest = (TextView) view.findViewById(R.id.textView);
-        tvTest.setText(String.format("expense:%d income:%d",clickBtSearch(1),clickBtSearch(2)));
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1, new float[]{clickBtSearch(1),clickBtSearch(2)}));
+        entries.add(new BarEntry(2, new float[]{clickBtSearch(3),clickBtSearch(4)}));
+        entries.add(new BarEntry(3, new float[]{clickBtSearch(5),clickBtSearch(6)}));
+
+        TextView tvTest = (TextView) view.findViewById(R.id.textView2);
+        tvTest.setText(String.format("expense:%d income:%d",a,b));
 
 
         // 棒グラフ表示用のデータセットに準備したリストをセット
@@ -122,7 +135,7 @@ public class Tab2Fragment extends Fragment {
         dataSet.setHighlightEnabled(false);
 
         // 棒グラフの色をセット
-        dataSet.setColors(new int[]{R.color.colorAccent, R.color.colorPrimary, R.color.colorPrimaryDark}, getActivity());
+        dataSet.setColors(new int[]{R.color.colorAccent, R.color.colorPrimary}, getActivity());
 
         List<IBarDataSet> bars = new ArrayList<>();
         bars.add(dataSet);
@@ -130,20 +143,51 @@ public class Tab2Fragment extends Fragment {
         return bars;
     }
 
+    private LineDataSet  getLineData(View view){
+        // 表示させるデータをリストに格納
+
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, clickBtSearch(7)));
+        entries.add(new Entry(2, clickBtSearch(8)));
+        entries.add(new Entry(3, clickBtSearch(9)));
+
+        // データセットの作成
+        LineDataSet dataSet = new LineDataSet(entries, "line");
+
+        // データセットのスタイル設定（任意）
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setValueTextSize(10f);
+
+        // データセットをリストに追加
+        List<LineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSet);
+
+        return dataSet;
+    }
+
     public int clickBtSearch(int flag) {
 
         // 収支の集計領域を準備する
-        int sumIncome = 0;  // 収入の集計領域
-        int sumExpense = 0; // 支出の集計領域
+        int sumIncomeSeven = 0;  // 収入の集計領域
+        int sumExpenseSeven = 0; // 支出の集計領域
 
-        // 対象期間の始まりと終わりを入力する
-        //EditText etStart = view.findViewById(R.id.etStart);     // Viewにプログラムでの変数名を割り当てる
-        //EditText etEnd = view.findViewById(R.id.etEnd);       // Viewにプログラムでの変数名を割り当てる
-        //String startDate = etStart.getText().toString();   // EditTextに入力した内容を文字列にして変数に渡す
-        //String endDate = etEnd.getText().toString();// EditTextに入力した内容を文字列にして変数に渡す
+        int sumIncomeEight = 0;  // 収入の集計領域
+        int sumExpenseEight = 0; // 支出の集計領域
 
-        String startDate = "2022/07/01";   // EditTextに入力した内容を文字列にして変数に渡す
-        String endDate = "2022/08/31";
+        int sumIncomeNine = 0;  // 収入の集計領域
+        int sumExpenseNine = 0; // 支出の集計領域
+
+
+
+        String startDateSeven = "2022/07/01";   // EditTextに入力した内容を文字列にして変数に渡す
+        String endDateSeven = "2022/07/31";
+
+        String startDateEight = "2022/08/01";   // EditTextに入力した内容を文字列にして変数に渡す
+        String endDateEight = "2022/08/31";
+
+        String startDateNine = "2022/09/01";   // EditTextに入力した内容を文字列にして変数に渡す
+        String endDateNine = "2022/09/31";
 
 
         // 表示用のList(fintechDataList)を用意する
@@ -161,14 +205,42 @@ public class Tab2Fragment extends Fragment {
         // 上記をtext.txtが終わるまで繰り返す
         for (FintechData fdata : parser.fintechObjects) {
             // transDateがstartDateから、endDateまでの日付のデータのみListに入れる
-            if (fdata.getTransDate().compareTo(startDate) >= 0) {
-                if (fdata.getTransDate().compareTo(endDate) <= 0) {
+            if (fdata.getTransDate().compareTo(startDateSeven) >= 0) {
+                if (fdata.getTransDate().compareTo(endDateSeven) <= 0) {
                     fintechDataList.add(fdata);
                     // 収支の合計を集計する
                     if (fdata.getAmount() >= 0) {
-                        sumIncome = sumIncome + fdata.getAmount();   // 収入の合計を求める
+                        sumIncomeSeven = sumIncomeSeven + fdata.getAmount();   // 収入の合計を求める
                     } else {
-                        sumExpense = sumExpense + fdata.getAmount(); // 支出の合計を求める
+                        sumExpenseSeven = sumExpenseSeven + fdata.getAmount(); // 支出の合計を求める
+                    }
+                }
+            }
+        }
+
+        for (FintechData fdata : parser.fintechObjects) {
+            // transDateがstartDateから、endDateまでの日付のデータのみListに入れる
+            if (fdata.getTransDate().compareTo(startDateEight) >= 0) {
+                if (fdata.getTransDate().compareTo(endDateEight) <= 0) {
+                    fintechDataList.add(fdata);
+                    // 収支の合計を集計する
+                    if (fdata.getAmount() >= 0) {
+                        sumIncomeEight = sumIncomeEight + fdata.getAmount();   // 収入の合計を求める
+                    } else {
+                        sumExpenseEight = sumExpenseEight + fdata.getAmount(); // 支出の合計を求める
+                    }
+                }
+            }
+        }
+        for(FintechData fdata : parser.fintechObjects){
+            if (fdata.getTransDate().compareTo(startDateNine) >= 0) {
+                if (fdata.getTransDate().compareTo(endDateNine) <= 0) {
+                    fintechDataList.add(fdata);
+                    // 収支の合計を集計する
+                    if (fdata.getAmount() >= 0) {
+                        sumIncomeNine = sumIncomeNine + fdata.getAmount();   // 収入の合計を求める
+                    } else {
+                        sumExpenseNine = sumExpenseNine + fdata.getAmount(); // 支出の合計を求める
                     }
                 }
             }
@@ -184,12 +256,36 @@ public class Tab2Fragment extends Fragment {
                 new int[]{R.id.tvList1, R.id.tvList2, R.id.tvList3} // 自作したレイアウトのViewのidを指定
         );
 
-        if (flag == 1){
-            return sumExpense;
-        }else {
-            return sumIncome;
-        }
+        int resSeven = sumExpenseSeven + sumIncomeSeven;//7月収支の合計
+        int resEight = sumExpenseEight + sumIncomeEight;//8月収支の合計
+        int resNine = sumExpenseNine + sumIncomeNine;//9月収支の合計
 
+
+        if (flag == 1){
+            return sumExpenseSeven;
+        }else if(flag == 2){
+            return sumIncomeSeven;
+        }
+        else if(flag == 3){
+            return sumExpenseEight;
+        }else if(flag == 4){
+            return sumIncomeEight;
+        }
+        else if(flag == 5){
+            return sumExpenseNine;
+        }else if(flag == 6){
+            return sumIncomeNine;
+        }
+        else if(flag == 7){
+            return sumExpenseSeven;
+        }else if(flag == 8){
+            return sumExpenseEight;
+        }else if(flag == 9){
+            return sumExpenseNine;
+        }
+        else {
+            return 0;
+        }
     }
 
     // Adapterに渡す形式のlist型変数の宣言と初期化
